823580b9e596e62e8e4286e6331812b2ee8d0a55
Durga Yamini <kodamanchiliyamini@gmail.com>
2022-05-05 17:44:32 +0530

    basic scheduler using timer class

---
+++ Timer.Class.Scheduler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Timers;
+
+namespace SchedulerWithTimer
+{
+    class Program
+    {
+        static Timer _timer = new Timer(1000);
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Timer starts!");
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Enabled = true;
+            _timer.AutoReset = true;
+            _timer.Start();
+            Console.ReadKey();
+        }
+
+        private static void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(++count + " seconds");
+        }
+    }
+}
